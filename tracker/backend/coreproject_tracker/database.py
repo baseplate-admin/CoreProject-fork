cb77bf0e79b41d03de23a6b71456c0722a3830f8
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-04-25 20:28:43 +0600

    add

--- tracker/backend/coreproject_tracker/database.py
+++ tracker/backend/coreproject_tracker/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
+from contextlib import asynccontextmanager
 
 from coreproject_tracker.envs import DATABASE_URI
 
@@ -7,3 +8,18 @@ engine = create_async_engine(DATABASE_URI, echo=True)
 
 # Session to be used throughout app.
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+
+@asynccontextmanager
+async def get_session():
+    """
+    Async contextmanager that yields a Session,
+    commits if all goes well, rolls back on error.
+    """
+    async with SessionLocal() as session:
+        try:
+            yield session
+            await session.commit()
+        except:
+            await session.rollback()
+            raise
