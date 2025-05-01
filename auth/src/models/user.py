bbf9aace3a786fd33952c8dd702a30dd8914b0db
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-05-01 13:06:36 +0600

    add

--- auth/src/models/user.py
+++
@@ -1,8 +0,0 @@
-import uuid
-from sqlmodel import Field, SQLModel
-
-
-class User(SQLModel, table=True):
-    id: uuid.UUID = Field(default=uuid.uuid4, primary_key=True)
-    name: str = Field(index=True, nullable=False)
-    email: str = Field(unique=True, nullable=False)
