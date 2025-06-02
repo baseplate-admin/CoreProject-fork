4a6730b34e104cec8442d9ba41a226c8b00b8c4c
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-06-02 10:58:43 +0600

    Add

--- auth/apps/users/managers.py
+++ auth/apps/users/managers.py
@@ -38,15 +38,16 @@ class UserManager(
         **extra_fields: Any,
     ) -> "CustomUser":
         """Create and save a SuperUser with the given email and password."""
-
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
+        for key in ("is_staff","is_superuser","is_active"):
+            extra_fields.setdefault(key, True)
 
         # Sanity Check
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError(_("Superuser must have is_staff=True."))
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
+        for key, value in {
+            "is_staff":_("Superuser must have is_staff=True."),
+            "is_superuser":_("Superuser must have is_superuser=True."),
+        }:
+            if extra_fields.get(key) is not True:
+                raise ValueError(value)
+    
 
         return self.create_user(email, password, **extra_fields)
